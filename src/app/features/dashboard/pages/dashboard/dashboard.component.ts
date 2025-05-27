9ed6a078b6462ca3e0c89652fad1118479027c54
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-05-26 23:19:39 -0500

    Reemplaza contenido de app.component.html con un componente de dashboard, elimina pruebas de app.component.spec.ts, actualiza app.component.ts para usar DashboardComponent y define rutas en app.routes.ts.

---
+++ src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderDashboardComponent } from '../../components/header-dashboard/header-dashboard.component';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [HeaderDashboardComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss'
+})
+export class DashboardComponent {
+
+}
