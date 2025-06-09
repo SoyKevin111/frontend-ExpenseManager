1c9275b8586c59d719b1c75096842f16341ae947
SoyKevin111 <kevinstevennn768xd@gmail.com>
2025-06-09 15:13:54 -0500

    Creando el componente de estado con el subcomponente de piechart usando ngxECharts

--- src/app/features/dashboard/components/my-state/my-state.component.ts
+++ src/app/features/dashboard/components/my-state/my-state.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PieChartComponent } from '../pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-my-state',
   standalone: true,
-  imports: [],
+  imports: [PieChartComponent],
   templateUrl: './my-state.component.html',
   styleUrl: './my-state.component.scss'
 })
--- src/app/features/dashboard/components/pie-chart/pie-chart.component.ts
+++ src/app/features/dashboard/components/pie-chart/pie-chart.component.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule, PLATFORM_ID } from '@angular/core';
+import { NgxEchartsModule } from 'ngx-echarts';
+import { EChartsOption } from 'echarts';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-pie-chart',
   standalone: true,
-  imports: [],
+  imports: [NgxEchartsModule, CommonModule],
   templateUrl: './pie-chart.component.html',
   styleUrl: './pie-chart.component.scss'
 })
 export class PieChartComponent {
+
+  readonly isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+
+  pieChartOption: EChartsOption = {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: 'center',
+      left: '10%',
+      orient: 'vertical',
+      icon: 'rect',  
+      itemWidth: 15,     
+      itemHeight: 15,     
+      textStyle: {
+        fontSize: 10      // tamaño del texto
+      }
+    },
+    series: [
+      {
+        name: 'Accesos',
+        type: 'pie',
+        radius: '70%',
+        center: ['65%', '47%'],
+        data: [
+          { value: 1048, name: 'Directo' },
+          { value: 735, name: 'Email' },
+          { value: 580, name: 'Publicidad' },
+          { value: 484, name: 'Búsqueda' },
+          { value: 300, name: 'Redes Sociales' }
+        ],
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        },
+        animation: true // animación activada (por defecto)
+      }
+    ]
+  };
+
 
 }
